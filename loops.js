3bd3528a836a3ad79e563f2811264d870591c6f4
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-27 19:42:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,5 +9,4 @@ function whileLoop(n) {
 +    return "done";
 +  }
 +
-function doWhileLoop(array) { function maybeTrue() { return Math.random() >= 0.5 } do { array.pop(); } while (array.length > -1 && maybeTrue()); return array;
-}
+function doWhileLoop(array) { function maybeTrue() { return Math.random() >= 0.5 } do { array.pop(); } while (array.length > -1 && maybeTrue()); return array; }
