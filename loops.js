b17a2b7d57607e254831bdfadc22e9fa2d19fa6a
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-27 20:03:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,15 +3,7 @@ function whileLoop(n) { let countdown = n; while (countdown > 0) { console.log(-
 function doWhileLoop(array) { function maybeTrue() { return Math.random() >= 0.5 } do { array.pop(); } while ( array.length > -1 && maybeTrue()); return array; } }
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop")
-    } else {
-      array.push(`I am ${i} strange loops.`)
-    }
-    }
-    return array
-  }
-
+    if (i === 1) { array.push("I am 1 strange loop") } else { array.push(`I am ${i} strange loops.`) } } return array }
 function whileLoop(number) {
   while (number > 0){
     console.log(number--)
@@ -19,13 +11,6 @@ function whileLoop(number) {
   return "done"
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-
-function doWhileLoop(array) {
-  do {
-    array = array.slice(1)
-  } while (array.length > 0 && maybeTrue())
-  return array
+function maybeTrue() { return Math.random() >= 0.5 }
+function doWhileLoop(array) { do { array = array.slice(1) } while (array.length > 0 && maybeTrue()) return array
 }
