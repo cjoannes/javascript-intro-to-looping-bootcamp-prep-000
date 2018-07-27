3a5533c07198559527b6f43465c64aa0b0363b3d
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-27 19:04:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,1 +1,1 @@
-for( let i = 0; i < 25; i++); { if i = 1; then console.log("I am 1 strange loop"); else console.log("I am ${+i+} strange loops"); }
+for ( let i = 0; i < 25; i++); { if i = 1; then console.log("I am 1 strange loop"); else console.log("I am ${+i+} strange loops"); }
